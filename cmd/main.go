ad5707d2efc1c369de77611bc97d77a8907d77ea
AzizRahimov <aziz.rahimov0001@gmail.com>
2020-10-12 18:32:19 +0500

    added new function

--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,24 @@ import (
 func main() {
 	s := &wallet.Service{}
 
-	_, err := s.RegisterAccount("+992938151007")
+	//_, err := s.RegisterAccount("+992938151007")
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//
+	//err = s.ExportToFile("data/export.txt")
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	err := s.ImportFromFile("data/export.txt")
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	err = s.ExportToFile("data/export.txt")
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	log.Println(s.FindAccountByID(1))
+
 }
+
